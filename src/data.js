37bdf38f1d6f1d8dc842507d399f8ed0300b5049
Aki Imai <akimai808@gmail.com>
2019-01-29 11:05:23 -0800

    update

---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Product #1",
+        img: "img/03.jpg",
+        price: 10,
+        company: "TEST",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Product #2",
+        img: "img/04.jpg",
+        price: 20,
+        company: "TEST",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Product #3",
+        img: "img/05.jpg",
+        price: 25,
+        company: "TEST",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Product #4",
+        img: "img/03.jpg",
+        price: 30,
+        company: "TEST",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "Product #5",
+        img: "img/04.jpg",
+        price: 35,
+        company: "TEST",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Product #6",
+        img: "img/05.jpg",
+        price: 40,
+        company: "TEST",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "Product #1",
+    img: "img/03.jpg",
+    price: 40,
+    company: "TEST",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    inCart: false,
+    count: 0,
+    total: 0
+};
